8e2b4d048be67b3a100755e6685012d3d7e9f3b9
Martin <luhrmanm@gmail.com>
2014-05-26 21:17:32 +0200

    inieminie aanpassinkje enzo

--- Themaopdacht/src/AccountSysteem/LoginServlet.java
+++ Themaopdacht/src/AccountSysteem/LoginServlet.java
@@ -16,7 +16,6 @@ public class LoginServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	@SuppressWarnings("static-access")
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		String username = req.getParameter("username");
