fe5c404ba3608a04539240875d38760937ab8814
Ingrid <childofnight92@gmail.com>
2014-06-05 10:20:15 +0200

    jdoc in package klantenbinding teruggezet

--- Themaopdacht/src/klantenbinding/Auto.java
+++ Themaopdacht/src/klantenbinding/Auto.java
@@ -4,6 +4,9 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+/**
+ * Deze klasse wordt gebruikt om de gegevens van een auto in te vullen of te bewerken
+ */
 public class Auto {
 	private String kenteken;
 	private String merk;
@@ -13,54 +16,105 @@ public class Auto {
 	DateFormat df = new SimpleDateFormat("dd/MM/yyyy"); // zo staat de datum als
 														// dag-maand-jaar ipv //
 														// Calendar....
-
+	/**
+	 * Deze methode maakt een auto aan en vult het kenteken, 
+	 * het merk van de auto en het klantid van de eigenaar in
+	 * @param kt is het kenteken
+	 * @param mk is het merk van de auto
+	 * @param id is het klantenid van de eigenaar
+	 */
 	public Auto(String kt, String mk, String id) {
 		setKenteken(kt);
 		setMerk(mk);
 		setKlantid(id);
 	}
 
+	/**
+	 * deze methode vult het klantid van de eigenaar in
+	 * @param klantid de klantid
+	 */
 	public void setKlantid(String klantid) {
 		this.klantid = klantid;
 	}
 
+	/**
+	 * deze methode vraagt de klantid van de eigenaar op
+	 * @return de klantid
+	 */
 	public String getKlantid() {
 		return klantid;
 	}
 
+	/**
+	 * deze methode vraagt het kenteken van de auto op
+	 * @return het kenteken
+	 */
 	public String getKenteken() {
 		return kenteken;
 	}
 
+	/**
+	 * deze methode vult het kenteken in
+	 * @param kt het kenteken
+	 */
 	public void setKenteken(String kt) {
 		kenteken = kt;
 	}
 
+	/**
+	 * deze methode vraagt het merk van de auto op
+	 * @return het auto merk
+	 */
 	public String getMerk() {
 		return merk;
 	}
 
+	/**
+	 * deze methode vult het merk van de auto in
+	 * @param mk het auto merk
+	 */
 	public void setMerk(String mk) {
 		merk = mk;
 	}
 
+	//het kenteken is al een string dus dit is eigenlijk onnodig
+	/**
+	 * deze methode geeft het kenteken weer in de vorm van een string
+	 * @return het kenteken
+	 */
 	public String toString() {
 		String s = getKenteken() + "";
 		return s;
 	}
 
+	/**
+	 * deze methode vraagt de datum van de laatste onderhoudsbeurt op
+	 * @return de datum van de laatste onderhoudsbeurt
+	 */
 	public String getLaatstOnderhouden() {
 		return df.format(laatstOnderhouden.getTime());
 	}
 
+	/**
+	 * deze methode vult de datum van de laatste onderhoudsbeurt in
+	 * @param lo de datum waarop de laatste onderhoudsbeurt geweest is
+	 */
 	public void setLaatstOnderhouden(Calendar lo) {
 		laatstOnderhouden = lo;
 	}
 
+	/**
+	 * deze boolean geeft aan of de auto op dit moment gerepareert wordt of niet
+	 * @return de boolean inReparatie
+	 */
 	public boolean isInReparatie() {
 		return inReparatie;
 	}
 
+	/**
+	 * deze methode vult de boolean inReparatie in
+	 * @param inReparatie een true of false waarde afhankelijk van of de auto gerepareert wordt
+	 */
 	public void setInReparatie(boolean inReparatie) {
 		this.inReparatie = inReparatie;
 	}
--- Themaopdacht/src/klantenbinding/Bedrijf.java
+++ Themaopdacht/src/klantenbinding/Bedrijf.java
@@ -3,21 +3,40 @@ package klantenbinding;
 import java.util.ArrayList;
 import voorraadbeheer.Voorraad;
 import Klusbeheer.Planner;
-
+/**
+ * deze klasse houdt de lijst van met alle klanten bij
+ * de naam van het bedrijf kan ingevuld/gewijzigd worden
+ * in deze klasse worden andere klasse met arraylists geinitialiseerd 
+ * zodat overal met dezelfde arraylists gewerkt wordt
+ */
 public class Bedrijf {
 	private ArrayList<Klant> alleKlanten = new ArrayList<Klant>();
 	private String bedrijfsNaam;
 	private Planner dePlanner;
 	private Voorraad deVoorraad;
 
+	/**
+	 * het bedrijf wordt aangemaakt en de bedrijfsnaam wordt opgegeven
+	 * @param nm de bedrijfsnaam
+	 */
 	public Bedrijf(String nm) {
 		setNaam(nm);
 	}
 
+	/**
+	 * de lijst met alle klanten wordt opgevraagt
+	 * @return de inhoud van de ArrayList alleKlanten
+	 */
 	public ArrayList<Klant> getAlleKlanten() {
 		return alleKlanten;
 	}
 
+	/**
+	 * deze methode voegt een klant toe aan de klantenlijst als deze er nog niet in stond
+	 * of een klant al in de lijst staat wordt gecontroleert dmv de methode heeftKlant()
+	 * @param nwK de toe te voegen klant
+	 * @return een boolean om aan te geven of het toevoegen gelukt is
+	 */
 	public boolean voegKlantToe(Klant nwK) {
 		if (heeftKlant(nwK.getEmail())) {
 			return false;
@@ -28,6 +47,12 @@ public class Bedrijf {
 		}
 	}
 
+	/**
+	 * er wordt mbv een emailadres en de methode zoekKlant() gekeken 
+	 * of een klant al in de klantenlijst staat
+	 * @param em het emailadres waar naar gezocht moet worden
+	 * @return een boolean om aan te geven of de klant in de ArrayList staat
+	 */
 	public boolean heeftKlant(String em) {
 		boolean result = false;
 		if (zoekKlant(em) != null) {
@@ -36,6 +61,11 @@ public class Bedrijf {
 		return result;
 	}
 
+	/**
+	 * deze methode wordt gebruikt om een klant te zoeken mbv het emailadres
+	 * @param em het emailadres van de te zoeken klant
+	 * @return de klant met het gezochte emailadres als de klant in de ArrayList staat
+	 */
 	public Klant zoekKlant(String em) {
 		Klant result = null;
 		for (Klant k : alleKlanten) {
@@ -46,6 +76,11 @@ public class Bedrijf {
 		return result;
 	}
 
+	/**
+	 * deze methode wordt gebruikt om een klant uit de ArrayList te halen
+	 * @param exk de te verwijderen klant
+	 * @return een boolean om aan te geven of het verwijderen gelukt is.
+	 */
 	public boolean verwijderKlant(Klant exk) {
 		boolean resultaat = false;
 		if (heeftKlant(exk.getEmail())) {// Klant kan alleen verwijdert worden
@@ -56,6 +91,10 @@ public class Bedrijf {
 		return resultaat;
 	}
 
+	/**
+	 * deze methode geeft weer hoeveel klanten er in de ArrayList alleKlanten staan
+	 * @return het aantal klanten
+	 */
 	public int aantalKlanten() {
 		if (alleKlanten.size() != 0) {
 			return alleKlanten.size();
@@ -65,14 +104,24 @@ public class Bedrijf {
 
 	}
 
+	/**
+	 * deze methode vraagt de bedrijfsnaam op
+	 * @return de bedrijfsnaam
+	 */
 	public String getNaam() {
 		return bedrijfsNaam;
 	}
 
+	/**
+	 * deze methode vult de bedrijfsnaam in
+	 * @param nm de bedrijfsnaam
+	 */
 	public void setNaam(String nm) {
 		bedrijfsNaam = nm;
 	}
 
+	//in de onderstaande methoden worden de klassen waarin arraylists worden gemaakt geinitialiseerd
+	//zodat er in elke klasse met dezelfde arraylists gewerkt worden
 	public Planner getDePlanner() {
 		return dePlanner;
 	}
@@ -92,7 +141,14 @@ public class Bedrijf {
 	public Planner getDeFinancien() {
 		return dePlanner;
 	}
+	
+	//het initialiseren is gestopt missen we niet een setDeFinancien?
 
+	/**
+	 * deze methode geeft een string weer waarin alle klanten worden weergegeven en 
+	 * hoeveel klanten er zijn
+	 * @return een string
+	 */
 	public String toString() {
 		String i = "";
 		for (Klant k : alleKlanten) {
