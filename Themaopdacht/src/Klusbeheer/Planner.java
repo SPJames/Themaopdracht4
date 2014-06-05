9dd6e4b941dc696d5c0f4ae0e8167a11b4fe1d44
Ingrid <childofnight92@gmail.com>
2014-06-05 10:44:24 +0200

    jdoc en logica commentaar teruggezet bij package klusbeheer

--- Themaopdacht/src/Klusbeheer/Planner.java
+++ Themaopdacht/src/Klusbeheer/Planner.java
@@ -2,13 +2,23 @@ package Klusbeheer;
 
 import java.util.ArrayList;
 
+/**
+ * in deze klasse worden alle weekplanningen bijgehouden
+ */
 public class Planner {
 	private ArrayList<Weekplanning> alleWeekplanningen = new ArrayList<Weekplanning>();
 
+	//de klasse wordt geinitialiseerd
 	public Planner() {
 
 	}
 
+	/**
+	 * deze methode kijkt mbv zoekplanning en een weeknummer of er een planning in de lijst
+	 * alleWeekplanningen staat voor die week
+	 * @param wn het weeknummer waarvoor we willen kijken of het een planning heeft
+	 * @return een boolean die aangeeft of er een planning is of niet
+	 */
 	public boolean heefPlanning(int wn) {
 		boolean result = false;
 		if (zoekPlanning(wn) != null) {
@@ -17,6 +27,12 @@ public class Planner {
 		return result;
 	}
 
+	/**
+	 * deze methode zoekt een planning in de arraylist alleWeekplanningen
+	 * er wordt gezocht op weeknummer
+	 * @param wn het weeknummer waarop gezocht wordt
+	 * @return de planning van die week als die er is, anders niks
+	 */
 	public Weekplanning zoekPlanning(int wn) {
 		Weekplanning result = null;
 		for (Weekplanning w : alleWeekplanningen) {
@@ -27,6 +43,11 @@ public class Planner {
 		return result;
 	}
 
+	/**
+	 * deze methode voegt een planning toe aan de arraylist alleWeekplanningen
+	 * @param nwP de toe te voegen weekplanningen
+	 * @return een boolean die aangeeft of het gelukt is om de planning toe te voegen
+	 */
 	public boolean voegPlanningToe(Weekplanning nwP) {
 		alleWeekplanningen.add(nwP);
 		return true;
