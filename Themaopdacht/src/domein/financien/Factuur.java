d5457cdab6d5cc20aa278cc04ab963a3694abd2e
Martin <luhrmanm@gmail.com>
2014-06-17 17:32:51 +0200

    Factuur code

--- Themaopdacht/src/domein/financien/Factuur.java
+++ Themaopdacht/src/domein/financien/Factuur.java
@@ -1,6 +1,11 @@
 package domein.financien;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import domein.klusbeheer.Klus;
+import domein.voorraadbeheer.Brandstof;
+import domein.voorraadbeheer.Onderdeel;
 
 /**
  * In de klasse Factuur wordt de factuur opgesteld
@@ -9,6 +14,11 @@ public class Factuur {
 	private Klus klus;
 	private static int nummer = 1;
 	private int factuurNummer;
+	
+	private double manurenprijs;
+	private HashMap<String, Double> onderdelenprijs = new HashMap<String, Double>();
+	private HashMap<String, Double> brandstofprijs = new HashMap<String, Double>();
+	private double totaalprijs;
 
 	/**
 	 * Er wordt een Factuur aangemaakt en meteen aan een klus gekoppelt
@@ -19,6 +29,62 @@ public class Factuur {
 	public Factuur(Klus k) {
 		setKlus(k);
 		factuurNummer = nummer++;
+		setManurenprijs(k);
+	}
+
+	public double getManurenprijs() {
+		return manurenprijs;
+	}
+
+	public void setManurenprijs(Klus k) {
+		double prijs = 0.0;
+		prijs = k.getManuren() * 0.0; // 0.0 = prijs per uur
+		
+		manurenprijs = prijs;
+	}
+
+	public HashMap<String, Double> getOnderdelenprijs() {
+		return onderdelenprijs;
+	}
+
+	public void setOnderdelenprijs(Klus k) {
+		HashMap<Onderdeel, Integer>gebruikteOnderdelen = k.getGebruikt();
+		for (Onderdeel o : gebruikteOnderdelen.keySet()) {
+			double a = gebruikteOnderdelen.get(o) * 0.0; // 0.0 = prijs per uur
+		    onderdelenprijs.put(o.getNaam(), a);
+		}
+	}
+
+	public HashMap<String, Double> getBrandstofprijs() {
+		return brandstofprijs;
+	}
+
+	public void setBrandstofprijs(Klus k) {
+		HashMap<Brandstof, Integer>gebruikteBrandstof = k.getGebruikteBrandstof(); // get de gebruikte brandstof
+		for (Brandstof b : gebruikteBrandstof.keySet()) {
+			double a = gebruikteBrandstof.get(b) * 0.0; // 0.0 = prijs per uur
+		    brandstofprijs.put(b.getBrandstofType(), a);
+		}
+	}
+
+	public double getTotaalprijs() {
+		return totaalprijs;
+	}
+
+	public void setTotaalprijs(Klus k) {
+		double prijs = 0.0;
+		
+		for (String o : onderdelenprijs.keySet()) {
+			prijs =+ onderdelenprijs.get(o);
+		}
+		
+		for (String b : brandstofprijs.keySet()) {
+			prijs =+ brandstofprijs.get(b);
+		}
+		
+		prijs =+ manurenprijs;
+		
+		totaalprijs = prijs;
 	}
 
 	/**
