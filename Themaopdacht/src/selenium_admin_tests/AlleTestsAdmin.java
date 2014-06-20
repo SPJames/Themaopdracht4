1ccabf7e908e17967826aa4046a9f5ebb8abe404
viddrawings <viddrawings@Cindy>
2014-06-20 18:53:51 +0200

    selenium voor admin toegevoegd

---
+++ Themaopdacht/src/selenium_admin_tests/AlleTestsAdmin.java
@@ -0,0 +1,11 @@
+package selenium_admin_tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ NieuweMonteurToevoegen.class })
+public class AlleTestsAdmin {
+	
+}
--- Themaopdacht/src/selenium_tests_Start/AlleTests_start.java
+++ Themaopdacht/src/selenium_tests_Start/AlleTests_start.java
@@ -4,11 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import selenium_klant_tests.*;
-import selenium_monteur_tests.*;
+import selenium_admin_tests.AlleTestsAdmin;
+import selenium_klant_tests.AlleTests;
+import selenium_monteur_tests.AlleTestsMonteur;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AlleTests.class, AlleTestsMonteur.class })
+@SuiteClasses({ AlleTests.class, AlleTestsMonteur.class, AlleTestsAdmin.class })
 public class AlleTests_start {
 
 }
