2ddbac6d18e62c8b4b55c592c2246717f536aa69
Martin <luhrmanm@gmail.com>
2014-06-24 15:59:27 +0200

    JUnit tests update

--- Themaopdacht/src/testPackage/AllTests.java
+++ Themaopdacht/src/testPackage/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BedrijfTest.class, KlantTest.class, AutoTest.class, BestellingTest.class, KlusTest.class, MonteurTest.class, ParkeerplaatsTest.class })
+@SuiteClasses({ KlantTest.class, AutoTest.class, KlusTest.class, MonteurTest.class, ParkeerplaatsTest.class })
 public class AllTests {
 
 }
