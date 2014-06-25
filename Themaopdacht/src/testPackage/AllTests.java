d4d6b75ce720cf101156dbed87187dd27efd7cd3
viddrawings <viddrawings@Cindy>
2014-06-25 14:06:23 +0200

    test junit, alltests

--- Themaopdacht/src/testPackage/AllTests.java
+++ Themaopdacht/src/testPackage/AllTests.java
@@ -5,7 +5,10 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ KlantTest.class, AutoTest.class, KlusTest.class, MonteurTest.class, ParkeerplaatsTest.class, FactuurTest.class, OnderhoudTest.class,ParkerenTest.class, TankenTest.class })
+@SuiteClasses({ KlantTest.class, AutoTest.class, KlusTest.class,
+		MonteurTest.class, ParkeerplaatsTest.class, FactuurTest.class,
+		OnderhoudTest.class, ParkerenTest.class, TankenTest.class,
+		OnderdeelTest.class, BrandstofTest.class })
 public class AllTests {
 
 }
